0ea6d9091f09b00dc87852b86917cbe64f55698a
behzodjon <behzodjontuit1998@gmail.com>
2023-07-21 18:05:19 +0500

    fix: bugs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
-import { UserModule } from './user/user.module';
+import { UsersModule } from './user/user.module';
 import { BookmarkModule } from './bookmark/bookmark.module';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
     AuthModule,
-    UserModule,
+    UsersModule,
     BookmarkModule,
-    MongooseModule,
+    MongooseModule.forRoot(
+      'mongodb://user:pass@127.0.0.1:37017',
+    ),
+    UsersModule,
   ],
 })
 export class AppModule {}
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,4 +1,20 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import {
+  User,
+  UserSchema,
+} from './schemas/user.schema';
+import { UsersController } from './user.controller';
+import { UsersRepository } from './users.repository';
+import { UsersService } from './users.service';
 
-@Module({})
-export class UserModule {}
+@Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: User.name, schema: UserSchema },
+    ]),
+  ],
+  controllers: [UsersController],
+  providers: [UsersService, UsersRepository],
+})
+export class UsersModule {}
